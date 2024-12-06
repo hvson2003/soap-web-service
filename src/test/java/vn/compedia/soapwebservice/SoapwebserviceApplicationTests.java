b5d4e1681057263a05ded600ad4598a17d498a7d
hvson03 <hvson2003@gmail.com>
2024-12-06 09:38:30 +0700

    first commit

---
+++ src/test/java/vn/compedia/soapwebservice/SoapwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.compedia.soapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
